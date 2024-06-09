7ff15e71740ad759bf0267274271985ecc8d7139
Varun Sharma <62187533+varunsh20@users.noreply.github.com>
2024-06-10 00:43:38 +0530

    Update riskPoolDeploy.js

--- contracts/hardhat/scripts/riskPoolDeploy.js
+++ contracts/hardhat/scripts/riskPoolDeploy.js
@@ -1,7 +1,7 @@
 const {ethers} = require("hardhat");
 
-const LIQUIDITY_VAULT = "0x22d17a4eBef6b2745E692440507F25E57cA15ac9"
-const ASSET_ADDRESSS = "0x52d800ca262522580cebad275395ca6e7598c014";
+const LIQUIDITY_VAULT = "0xd3EA3Ba54a6613D2441c0ec176354d1d5467FE3f"
+const ASSET_ADDRESSS = "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8";
 const main = async()=>{
     const contract = await ethers.getContractFactory("RiskPool");
     const deployContract = await contract.deploy(ASSET_ADDRESSS,LIQUIDITY_VAULT);
@@ -16,4 +16,5 @@ main()
     process.exit(1);
 });
 
-//0xB5b4f5245849DBb8a50F715Ae1486a0DE99a4f7c
+
+//0xcdCE7F23455e468c14b8F32c303DE36f6f3E7025
