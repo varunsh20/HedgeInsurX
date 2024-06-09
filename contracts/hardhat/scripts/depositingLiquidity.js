df8144b82d52549122556ae79a8c24ff404d08df
Varun Sharma <62187533+varunsh20@users.noreply.github.com>
2024-06-10 00:43:09 +0530

    Update depositingLiquidity.js

--- contracts/hardhat/scripts/depositingLiquidity.js
+++ contracts/hardhat/scripts/depositingLiquidity.js
@@ -7,14 +7,14 @@ async function main() {
     const privateKey = process.env.MUMBAI_PRIVATE_KEY;
     const wallet = new ethers.Wallet(privateKey, provider);
     const usdcABI = ['function approve(address spender, uint256 amount) returns (bool)'];
-    const usdcAddress = "0x52d800ca262522580cebad275395ca6e7598c014";
+    const usdcAddress = "0xF4cc1F9cf050eBded41dC62ddD1396D7Dd8b5BCa";
     const usdcContract = new ethers.Contract(usdcAddress,usdcABI,wallet);
     const amount =  ethers.parseUnits("200", 6); // 6 decimal places for USDC   
-    //const approveTx = await usdcContract.approve("0x5f96a7394eC5c2c8aC0AADa7B047841D16fFbcB3", amount);
-    const contractAddress = "0x22d17a4eBef6b2745E692440507F25E57cA15ac9";
+    const approveTx = await usdcContract.approve("0x5f96a7394eC5c2c8aC0AADa7B047841D16fFbcB3", amount);
+    const contractAddress = "0x5B6790946555a9B52b0dD6cC4603994562c65e31";
     const myContract = await ethers.getContractAt("LiquidityTokenizedVault", contractAddress,wallet);
-    //const tx = await myContract.deposit(amount,"0xA540e0e41c4141009800a257255907F578Dc1815");
-    const tx = await myContract.balanceOf("0xA540e0e41c4141009800a257255907F578Dc1815");
+    const tx = await myContract.deposit(amount,"0x4E48B4580775d83d160FF583816356e1e4c2915b");
+    const tx1 = await myContract.balanceOf("0x4E48B4580775d83d160FF583816356e1e4c2915b");
     console.log("Trx hash:", tx);
 }
 
